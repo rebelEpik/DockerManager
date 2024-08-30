d7fbc81aa22d1ce21d91036da79ce82fa23f50f8
Bryan Singleton <bryan.j.singleton@gmail.com>
2024-08-30 01:35:53 -0400

    Created Preferences.settings Added controls to Preferences window
    TODO:
    Save settings on preferences window functionality
    Finish skinning preferences window

--- DockerManager/Preferences.Designer.cs
+++ DockerManager/Preferences.Designer.cs
@@ -28,41 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabControl1 = new TabControl();
+            preferencesBottomButtonPanel = new Panel();
+            preferencesSaveButton = new Button();
+            preferencesCancelButton = new Button();
+            preferencesTabControl = new TabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
-            tabControl1.SuspendLayout();
+            preferencesBottomButtonPanel.SuspendLayout();
+            preferencesTabControl.SuspendLayout();
             SuspendLayout();
             // 
-            // tabControl1
+            // preferencesBottomButtonPanel
             // 
-            tabControl1.Alignment = TabAlignment.Left;
-            tabControl1.Controls.Add(tabPage1);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Dock = DockStyle.Fill;
-            tabControl1.Location = new Point(0, 0);
-            tabControl1.Multiline = true;
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(717, 478);
-            tabControl1.TabIndex = 0;
+            preferencesBottomButtonPanel.Controls.Add(preferencesSaveButton);
+            preferencesBottomButtonPanel.Controls.Add(preferencesCancelButton);
+            preferencesBottomButtonPanel.Dock = DockStyle.Bottom;
+            preferencesBottomButtonPanel.Location = new Point(0, 442);
+            preferencesBottomButtonPanel.Name = "preferencesBottomButtonPanel";
+            preferencesBottomButtonPanel.Size = new Size(717, 36);
+            preferencesBottomButtonPanel.TabIndex = 0;
+            // 
+            // preferencesSaveButton
+            // 
+            preferencesSaveButton.Location = new Point(527, 3);
+            preferencesSaveButton.Name = "preferencesSaveButton";
+            preferencesSaveButton.Size = new Size(86, 28);
+            preferencesSaveButton.TabIndex = 1;
+            preferencesSaveButton.Text = "Save";
+            preferencesSaveButton.UseVisualStyleBackColor = true;
+            preferencesSaveButton.Click += PreferencesSaveButton_Click;
+            // 
+            // preferencesCancelButton
+            // 
+            preferencesCancelButton.Location = new Point(619, 3);
+            preferencesCancelButton.Name = "preferencesCancelButton";
+            preferencesCancelButton.Size = new Size(86, 28);
+            preferencesCancelButton.TabIndex = 0;
+            preferencesCancelButton.Text = "Cancel";
+            preferencesCancelButton.UseVisualStyleBackColor = true;
+            preferencesCancelButton.Click += PreferencesCancelButton_Click;
+            // 
+            // preferencesTabControl
+            // 
+            preferencesTabControl.Alignment = TabAlignment.Left;
+            preferencesTabControl.Controls.Add(tabPage1);
+            preferencesTabControl.Controls.Add(tabPage2);
+            preferencesTabControl.Dock = DockStyle.Fill;
+            preferencesTabControl.DrawMode = TabDrawMode.OwnerDrawFixed;
+            preferencesTabControl.ItemSize = new Size(75, 97);
+            preferencesTabControl.Location = new Point(0, 0);
+            preferencesTabControl.Multiline = true;
+            preferencesTabControl.Name = "preferencesTabControl";
+            preferencesTabControl.SelectedIndex = 0;
+            preferencesTabControl.Size = new Size(717, 442);
+            preferencesTabControl.TabIndex = 1;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(27, 4);
+            tabPage1.Location = new Point(101, 4);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(686, 470);
+            tabPage1.Size = new Size(612, 434);
             tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
+            tabPage1.Text = "General Settings";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(27, 4);
+            tabPage2.Location = new Point(101, 4);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(686, 470);
+            tabPage2.Size = new Size(612, 434);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
@@ -72,16 +107,21 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(717, 478);
-            Controls.Add(tabControl1);
+            Controls.Add(preferencesTabControl);
+            Controls.Add(preferencesBottomButtonPanel);
             Name = "Preferences";
             Text = "Preferences";
-            tabControl1.ResumeLayout(false);
+            preferencesBottomButtonPanel.ResumeLayout(false);
+            preferencesTabControl.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TabControl tabControl1;
+        private Panel preferencesBottomButtonPanel;
+        private Button preferencesSaveButton;
+        private Button preferencesCancelButton;
+        private TabControl preferencesTabControl;
         private TabPage tabPage1;
         private TabPage tabPage2;
     }
--- DockerManager/Preferences.cs
+++ DockerManager/Preferences.cs
@@ -10,11 +10,49 @@ using System.Windows.Forms;
 
 namespace DockerManager
 {
-    public partial class Preferences: Form
+    public partial class Preferences : Form
     {
         public Preferences()
         {
             InitializeComponent();
+            preferencesTabControl.DrawItem += new DrawItemEventHandler(PreferencesTabControl_DrawItem);
+            preferencesTabControl.SizeMode = TabSizeMode.Fixed;
+            preferencesTabControl.ItemSize = new Size(75, preferencesTabControl.ItemSize.Height);
+        }
+
+        private void PreferencesCancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PreferencesSaveButton_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PreferencesTabControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush textBrush;
+            TabPage generalTabPage = preferencesTabControl.TabPages[e.Index];
+            Rectangle tabBounds = preferencesTabControl.GetTabRect(e.Index);
+
+            if(e.State == DrawItemState.Selected)
+            {
+                textBrush = new SolidBrush(Color.Black);
+                g.FillRectangle(Brushes.LightGray, e.Bounds);
+            }
+            else
+            {
+                textBrush = new SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            Font tabFont = new Font("Arial", 10.0f, FontStyle.Bold, GraphicsUnit.Pixel);
+            StringFormat stringFlags = new StringFormat();
+            stringFlags.Alignment = StringAlignment.Center;
+            stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(generalTabPage.Text, tabFont, textBrush, tabBounds, stringFlags);
         }
     }
 }
